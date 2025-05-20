23454772f887b5a82648c3455005eb63e401eb20
RmaUriel <uhernandez11404@gmail.com>
2025-05-20 14:49:49 -0700

    Lab Work done 5/20

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.swing.*;
+import java.awt.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main extends JFrame {
+
+    public Main(){
+        setTitle("Pong Game");
+        setSize( 1000,800);
+        setLayout(new BorderLayout());
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args){
+        Main game = new Main();
+        game.setVisible(true);
+
+        T4AField field = new T4AField();
+
     }
 }
